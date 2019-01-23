0f7f8f5985bd88f5dc0d37cf8a8ca7fdac9572dd
LenKIM <joenggyu0@gmail.com>
2019-01-23 17:09:11 +0900

    docs: add Item4

--- EffectiveJavaCode/src/Item4.java
+++ EffectiveJavaCode/src/Item4.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Item4 {
 
-public class item4 {
+    private Item4() {
+        System.out.println("constructor called");
+    }
+
+    public static int convertStringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public static void main(String[] args) {
+        Item4 instance = new Item4();
+        System.out.println(Item4.convertStringToInt("1"));
+    }
 }
+
